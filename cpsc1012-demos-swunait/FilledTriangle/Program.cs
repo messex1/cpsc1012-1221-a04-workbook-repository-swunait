a03792a01589eeb190331c4a5c15eca92f26f9d6
Sam Wu <swu@nait.ca>
2022-10-07 08:59:27 -0600

    Added FilledTriangle

---
+++ cpsc1012-demos-swunait/FilledTriangle/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+	Trace Table for Filled Triangle with a base of 5
+
+	Row		Column		Column <= Row	Output	
+	---		------		-------------	------
+	1		
+			1			1 <= 1 is T		*
+			2			2 <= 1 is F
+	2
+			1			1 <= 2 is T		*	
+			2			2 <= 2 is T		 *
+			3			3 <= 2 is F	
+										** (actual line output)
+	3			
+			1			1 <= 3 is T		*	
+			2			2 <= 3 is T		 *
+			3			3 <= 3 is T		  *
+			4			4 <= 3 is F	
+										*** (actual line output)
+	4
+			1		1 <= 4 is T	*	
+			2		2 <= 4 is T	 *
+			3		3 <= 4 is T	  *
+			4		4 <= 4 is T	   *	
+			5		5 <= 4 is F	
+										**** (actual line output)
+	5
+			1		1 <= 5 is T	*	
+			2		2 <= 5 is T	 *
+			3		3 <= 5 is T	  *
+			4		4 <= 5 is T	   *
+			5		5 <= 5 is T	    *
+										***** (actual line output)
+ * 
+ * 
+ * 
+ * 
+ * */
+int triangleBase = 15;
+
+for (int currentRow = 1; currentRow <= triangleBase; currentRow += 1)
+{
+    for (int currentColumn = 1; currentColumn <= currentRow;  currentColumn += 1)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}

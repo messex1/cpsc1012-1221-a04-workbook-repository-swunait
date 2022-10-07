866b697732dc361554bd9ed14c2bf32b65e534ac
Sam Wu <swu@nait.ca>
2022-10-07 09:12:23 -0600

    Added Truth Table to FilledRectangle

--- cpsc1012-demos-swunait/FilledRectangle/Program.cs
+++ cpsc1012-demos-swunait/FilledRectangle/Program.cs
@@ -1,6 +1,32 @@
 ﻿/* Purpose: Print a rectangle shape on the screen with a specific length (columns)
  *          and width (rows)
- * 
+Trace Table for Filled Rectangle with a length(column) of 5 and width(row) of 3
+
+	Row 	Column		Column <= Length	Output	
+	---		------		----------------	------
+	1		
+			1			1 <= 5 is T			*
+			2			2 <= 5 is T			 *
+			3			3 <= 5 is T			  *
+			4			4 <= 5 is T			   *
+			5			5 <= 5 is T			    *
+											***** (end result)
+	2
+			1			1 <= 5 is T			*
+			2			2 <= 5 is T			 *
+			3			3 <= 5 is T			  *
+			4			4 <= 5 is T			   *
+			5			5 <= 5 is T			    *							
+											***** (end result)
+	3			
+			1			1 <= 5 is T			*
+			2			2 <= 5 is T			 *
+			3			3 <= 5 is T			  *
+			4			4 <= 5 is T			   *
+			5			5 <= 5 is T			    *
+											***** (end result)
+											
+											
  * */
 
 int columns = 25;

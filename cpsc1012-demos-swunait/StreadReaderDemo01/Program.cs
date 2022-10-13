e2caf7f3aa147ce20a35ad7e37ae993507480e6e
Sam Wu <swu@nait.ca>
2022-10-13 11:41:56 -0600

    Added File and Try Catch demo projects

---
+++ cpsc1012-demos-swunait/StreadReaderDemo01/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("This program computes the average for a series of marks from a text file.");
+
+int sum = 0;
+int countMarks = 0;
+const string DataFilePath = @"..\..\..\marks.txt";
+
+try
+{
+    using (StreamReader reader = new StreamReader(DataFilePath))
+    {
+        string line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            int currentMark = int.Parse(line);
+            sum += currentMark;
+            countMarks++;
+        }
+        // Compute the avearge
+        double average = sum / countMarks;
+        Console.WriteLine($"Average of the {countMarks} marks is {average:F1}");
+
+    }
+} 
+catch (Exception ex)
+{
+    Console.WriteLine($"Error reading from {DataFilePath} with exception {ex.Message}");
+}
---
+++ cpsc1012-demos-swunait/StreamWriterDemo01/Program.cs
@@ -0,0 +1,48 @@
+﻿// Display a welcome message
+Console.WriteLine("This program writes a series of user input marks to a file.");
+
+const string DataFilePath = @"..\..\..\marks.txt";
+try
+{
+    using (StreamWriter writer = new StreamWriter(DataFilePath))
+    {
+        int mark = 0;
+
+        do
+        {
+            //bool validMark = false;
+
+            //while (!validMark)
+            //{
+            //    // Prompt for a mark
+            //    Console.Write("Enter a mark between 0 and 100: ");
+            //    try
+            //    {
+            //        mark = int.Parse(Console.ReadLine());
+            //        validMark = true;
+            //    }
+            //    catch
+            //    {
+            //        Console.WriteLine("Invalid input value. Please enter an integer number.");
+            //    }
+            //}
+            Console.Write("Enter a mark between 0 and 100: ");
+            while ( ! int.TryParse(Console.ReadLine(), out mark))
+            {
+                Console.WriteLine("Invalid input value. Try again.");
+                Console.Write("Enter a mark between 0 and 100: ");
+            }
+
+
+            // Write the mark to the file if it is a valid mark
+            if (mark >= 0 && mark <= 100)
+            {
+                writer.WriteLine(mark);
+            }
+        } while (mark >= 0 && mark <= 100);
+    }
+} 
+catch(Exception ex)
+{
+    Console.WriteLine($"Error writing to file {DataFilePath} with exception {ex.Message}");
+}

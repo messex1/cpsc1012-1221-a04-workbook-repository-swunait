7bbaff6daee698bf386f6c967e16e3a5aeb9d43a
Sam Wu <swu@nait.ca>
2022-10-31 11:51:19 -0600

    Add solution to ArrayExericse 1

---
+++ cpsc1012-demos-swunait/AverageScoreApp/Program.cs
@@ -0,0 +1,24 @@
+﻿// Finds the average of up to 100 scores.
+const int MAX_NUM_SCORES = 100; // Max number of scores
+int count; // Actual number of scores
+double[] score = new double[MAX_NUM_SCORES]; // Array of size
+double sum = 0; // Sum of array elements
+string userInput; // To hold user keyboard input value
+                  // Prompt the user for the number of scores.
+Console.WriteLine("Enter the number of scores: ");
+userInput = Console.ReadLine();
+count = int.Parse(userInput);
+// Read in the scores and store them in the array.
+Console.WriteLine("Enter the scores: ");
+for (int index = 0; index < count; index++)
+{
+    userInput = Console.ReadLine();
+    score[index] = double.Parse(userInput);
+}
+Console.WriteLine();
+// Find and display the average of the scores
+for (int index = 0; index < count; index++)
+{
+    sum += score[index]; // Sum the scores
+}
+Console.WriteLine($"The average is {sum / count}");
---
+++ cpsc1012-demos-swunait/PollutionLevelApp/Program.cs
@@ -0,0 +1,21 @@
+﻿const int POLLUTION_ARRAY_SIZE = 6;
+// Array of six pollution level readings
+int[] pollutionLevel = new int[POLLUTION_ARRAY_SIZE];
+string userInput; // To hold user keyboard input value
+                  // Input six integers into the pollution level array.
+Console.WriteLine("Enter the six pollution level readings: ");
+for (int index = 0; index < POLLUTION_ARRAY_SIZE; index++)
+{
+    userInput = Console.ReadLine();
+    pollutionLevel[index] = int.Parse(userInput);
+}
+// Display the readings three values per line.
+for (int index = 0; index < POLLUTION_ARRAY_SIZE; index++)
+{
+    if (index % 3 == 0)
+    {
+        Console.WriteLine();
+    }
+    Console.Write($"{pollutionLevel[index]} ");
+}
+Console.WriteLine();

2d138e9732ab8e8d14141bf8bb21a57b6eb60f43
Sam Wu <swu@nait.ca>
2022-11-03 11:13:16 -0600

    Added Array Exercise 2 Warmup

---
+++ cpsc1012-demos-swunait/PassingArraysToMethods/Program.cs
@@ -0,0 +1,56 @@
+﻿int numValues; // Actual number of values to process
+int[] value = new int[MAX_NUM_VALUES]; // Array of integers
+string userInput; // To hold user keyboard input value
+                  // Prompt the user for the number of data items.
+Console.Write("Enter the number of values to process: ");
+userInput = Console.ReadLine();
+numValues = int.Parse(userInput);
+// Read in the data.
+InputValues(value, numValues);
+// Double the value of each data element.
+DoubleValues(value, numValues);
+// Display the data.
+Console.WriteLine("Data doubled:");
+DisplayValues(value, numValues);
+Console.WriteLine();
+
+// --------------------------------------------------------------------
+static void InputValues(int[] data, int count)
+{ // Read the values into an array
+    Console.WriteLine("Enter the data: ");
+    string userInput; // To hold user keyboard input value
+    for (int index = 0; index < count; index++)
+    {
+        userInput = Console.ReadLine();
+        data[index] = int.Parse(userInput);
+    }
+}
+
+// --------------------------------------------------------------------
+ static void DoubleValues(int[] data, int count)
+{ // Double the values in the array
+    for (int index = 0; index < count; index++)
+    {
+        data[index] = data[index] * 2;
+    }
+}
+
+// --------------------------------------------------------------------
+static void DisplayValues(int[] data, int count)
+{ // Display the values in the array.
+    for (int index = 0; index < count; index++)
+    {
+        if (index % MAX_PER_LINE == 0)
+        {
+            Console.WriteLine();
+        }
+        Console.Write($"{data[index]}");
+    }
+}
+
+public partial class Program
+{
+    const int MAX_NUM_VALUES = 20; // Max number of data values
+    const int MAX_PER_LINE = 5; // Max values per line to display
+
+}
---
+++ cpsc1012-demos-swunait/PassingArraysToMethodsWarmUp/Program.cs
@@ -0,0 +1,50 @@
+﻿int numTrips; // Actual number of trips
+double[] miles = new double[MAX_TRIPS]; // Miles data
+double[] kms = new double[MAX_TRIPS]; // Kilometers data
+string userInput; // To hold user keyboard input value
+                  // Prompt the user for the number of trips.
+Console.WriteLine("Enter the number of trips: ");
+userInput = Console.ReadLine();
+numTrips = int.Parse(userInput);
+// Read the mileage for each trip.
+ReadMiles(miles, numTrips);
+// Convert from miles to kilometers.
+MilesToKms(miles, kms, numTrips);
+// Display the data for each trip.
+DisplayData(miles, kms, numTrips);
+
+// --------------------------------------------------------------------
+static void ReadMiles(double[] tripMiles, int count)
+{
+    string userInput; // To hold user input value
+    Console.WriteLine("Enter the mileage for each trip: ");
+    for (int index = 0; index < count; index++)
+    {
+        userInput = Console.ReadLine();
+        // Store the userInput in the tripMiles array
+        tripMiles[index] = double.Parse(userInput);
+    }
+}
+static void MilesToKms(double[] tripMiles, double[] tripKms, int count)
+{
+    for (int index = 0; index < count; index++)
+    {
+        tripKms[index] = tripMiles[index] * MILES_TO_KMS;
+    }
+}
+static void DisplayData(double[] tripMiles, double[] tripKms, int count)
+{
+    Console.WriteLine($"{"Trip",4} {"Miles",7} {"Kilometers",12}");
+    for (int index = 0; index < count; index++)
+    {
+        Console.WriteLine($"{index + 1,4} {tripMiles[index],7} {tripKms[index],12}"
+        );
+    }
+}
+
+public partial class Program
+{
+    const int MAX_TRIPS = 100; // Max number of trips
+    const double MILES_TO_KMS = 1.61; // Miles to kilometers conversion factor
+
+}

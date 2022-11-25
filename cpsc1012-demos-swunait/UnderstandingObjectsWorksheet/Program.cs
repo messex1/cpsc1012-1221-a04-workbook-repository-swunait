f38c67d2cda7af07bb978beabb7409c79125aa19
Sam Wu <swu@nait.ca>
2022-11-25 09:45:53 -0700

    Added UnderstandingObjectsWorksheet

---
+++ cpsc1012-demos-swunait/UnderstandingObjectsWorksheet/Program.cs
@@ -0,0 +1,44 @@
+﻿using UnderstandingObjectsWorksheet;
+
+int quizTotal;
+int quizWeight;
+string studentName;
+int studentMark;
+
+List<Quiz> quizzes = new List<Quiz>();
+char addAnotherEntry = 'n';
+
+Console.Write("Enter quiz total: ");
+quizTotal = int.Parse(Console.ReadLine());
+Console.Write("Enter quiz weight: ");
+quizWeight = int.Parse(Console.ReadLine());
+
+do
+{
+    Console.Write("Enter student name: ");
+    studentName = Console.ReadLine();
+    Console.Write("Enter student mark: ");
+    studentMark = int.Parse(Console.ReadLine());
+    // Create a new Quiz object
+    Quiz currentQuiz = new Quiz(studentMark, quizTotal, quizWeight, studentName);
+    // Add the Quiz object to the list of quizzes
+    quizzes.Add(currentQuiz);
+    // Check if user wants to add another entry
+    Console.Write("Add another entry (Y|N): ");
+    addAnotherEntry = char.Parse(Console.ReadLine());
+    addAnotherEntry = char.ToLower(addAnotherEntry);
+} while (addAnotherEntry == 'y');
+
+Console.WriteLine($"{"Total",-15} {"Weight",-5}");
+Console.WriteLine($"{quizTotal,-15} {quizWeight,-5}");
+
+Console.WriteLine($"{"Name",-15} {"Mark",-5} {"Percentage",-10}");
+int markSum = 0;
+foreach(Quiz currentQuiz in quizzes)
+{
+    Console.WriteLine($"{currentQuiz.StudentName,-15} {currentQuiz.Mark,-5} {currentQuiz.GetPercentage():F2}%");
+    markSum += currentQuiz.Mark;
+}
+double averageMark = markSum / quizzes.Count;
+Console.WriteLine($"Average mark: {averageMark:F2}%");
+
---
+++ cpsc1012-demos-swunait/UnderstandingObjectsWorksheet/Quiz.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingObjectsWorksheet
+{
+    internal class Quiz
+    {
+        //public string StudentName { get; set; }
+        private string _studentName;
+
+        private int _total;
+        private int _mark;
+        private int _weight;
+
+        public string StudentName
+        {
+            get
+            {
+                return _studentName;
+            }
+            set
+            {
+                // Validate that the new value is not an empty string or a string with only whitespaces (space, tab)
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("StudentName is required");
+                }
+                // Validate that the new value contains at minimum 3 characters in length
+                if (value.Trim().Length < 3)
+                {
+                    throw new ArgumentException("StudentName must contain at minimum 3 characters");
+                }
+                _studentName = value.Trim();
+            }
+        }
+
+        public int Total
+        {
+            get => _total;
+            set => _total = value;
+        }
+        public int Mark
+        {
+            get => _mark;
+            set => _mark = value;
+        }
+        public int Weight
+        {
+            get => _weight;
+            set => _weight = value;
+        }
+
+        public Quiz(int Mark, int Total, int Weight, string StudentName)
+        {
+            this.Mark = Mark;
+            this.Total = Total;
+            this.Weight = Weight;
+            this.StudentName = StudentName;
+        }
+
+        public double GetPercentage()
+        {
+            // return ((double) Mark / Total) * 100;
+            return ((double) _mark / _total) * 100;
+        }
+    }
+}
